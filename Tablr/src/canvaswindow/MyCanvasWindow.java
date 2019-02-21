ec9dd5c233684b95d05b78e7b2787e361199bc72
Ignace Bleukx <ignace.bleukx@student.Kuleuven.be>
2019-02-21 15:48:56 +0100

    eerste geklooi

---
+++ Tablr/src/canvaswindow/MyCanvasWindow.java
@@ -0,0 +1,26 @@
+package canvaswindow;
+
+import java.awt.Graphics;
+
+public class MyCanvasWindow extends CanvasWindow {
+
+	protected MyCanvasWindow(String title) {
+		super(title);
+		// TODO Auto-generated constructor stub
+	}
+	
+	@Override
+	public void paint(Graphics g){
+		
+	}
+	
+	@Override
+	public void handleMouseEvent(int id, int x, int y, int clickCount){
+		
+	}
+
+	@Override
+	public void handleKeyEvent(int id, int keyCode, char keyChar){
+		
+	}
+}
---
+++ Tablr/src/canvaswindow/Program.java
@@ -0,0 +1,18 @@
+package canvaswindow;
+import java.awt.Graphics;
+
+
+public class Program {
+
+	public static void main(String[] args) {
+		      java.awt.EventQueue.invokeLater(() -> {
+		          //new MyCanvasWindow("My Canvas Window").show();
+		          Graphics rect  = g.
+		          rect.drawRect(50, 50, 100, 200);
+		          
+		      });
+	}
+	
+	private static Graphics g = 
+	
+}
