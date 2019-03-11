06f4921b99f14e1db0708bc07e9f2aad9c5986a9
Ignace Bleukx <ignace.bleukx@student.Kuleuven.be>
2019-03-11 14:19:05 +0100

    commit before pushing

--- Tablr/src/domain/Column.java
+++ Tablr/src/domain/Column.java
@@ -193,6 +193,7 @@ public class Column extends DomainElement {
 			return;
 		}
 		if (isValidValue(getColumnType(),v.toString())) this.defaultValue = v;
+		else throw new IllegalArgumentException();
 	}
 		
 	
